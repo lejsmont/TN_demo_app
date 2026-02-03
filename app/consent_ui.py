81d64cf1241e1bfe401b914ac2948540f601f0c9
Ludwik Ejsmont <ludwik.ejsmont@proton.me>
2026-02-03 21:03:27 +0100

    resolved issue with consumer key

--- app/consent_ui.py
+++ app/consent_ui.py
@@ -20,11 +20,6 @@ def consent_ui_src() -> str:
     return os.getenv("MC_CONSENT_UI_SRC", "https://consents.mastercard.com")
 
 
-def consent_ui_kid(consumer_key: str) -> str:
-    if "!" in consumer_key:
-        return consumer_key.split("!", 1)[0]
-    return consumer_key
-
 
 def generate_consent_ui_jwt(callback_origin: str) -> str:
     consumer_key = os.getenv("MC_CONSUMER_KEY")
@@ -51,7 +46,7 @@ def generate_consent_ui_jwt(callback_origin: str) -> str:
         payload,
         signing_key,
         algorithm="RS256",
-        headers={"typ": "JWT", "alg": "RS256", "kid": consent_ui_kid(consumer_key)},
+        headers={"typ": "JWT", "alg": "RS256", "kid": consumer_key},
     )
     return token
 
